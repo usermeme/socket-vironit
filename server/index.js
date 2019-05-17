aed84625c75fb33ba86be278703b2c39311bddc1
usermeme <romanyankovich98@gmail.com>
2019-05-17 16:57:47 +0300

    feat: create server & add answer-function

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 const net = require('net');
 
-const server = net.createServer((c) => {
-  
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+const answer = {
+	hello: 'world',
+	end: 'end',
+};
+
+const server = net.createServer((client) => {
+  console.log(`CONNECTED: ${client.remoteAddress}:${client.remotePort}`);
+
+  client.on('data', (data) => {
+    const result = answer[data.toString()] || 'incorrect input';
+    client.write(result);
+  });
 });
+server.listen(PORT, HOST);
