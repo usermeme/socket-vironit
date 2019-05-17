f0064b4c958fb5e6d963a46db7e3b65bcfc5d3f8
usermeme <romanyankovich98@gmail.com>
2019-05-17 19:58:25 +0300

    feat: delete server when one is empty

--- server/index.js
+++ server/index.js
@@ -4,16 +4,23 @@ const HOST = '127.0.0.1';
 const PORT = 8080;
 
 const answer = {
-	hello: 'world',
-	end: 'end',
+  list: [],
+  end: 'end',
+  hello: 'world',
+  help: Object.keys(this),
+  start_message: 'enter address',
 };
 
 const server = net.createServer((client) => {
   console.log(`CONNECTED: ${client.remoteAddress}:${client.remotePort}`);
 
+  answer.list.push(`${client.remoteAddress}:${client.remotePort}`);
   client.on('data', (data) => {
-    const result = answer[data.toString()] || 'incorrect input';
+    const result = `${answer[data.toString()]}` || 'incorrect input';
     client.write(result);
+
+    server.close();
   });
 });
+
 server.listen(PORT, HOST);
