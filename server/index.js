58e5673d353a1a5beec46c0b5a979e134db2abf8
usermeme <romanyankovich98@gmail.com>
2019-05-20 13:02:04 +0300

    feat: add help & list

--- server/index.js
+++ server/index.js
@@ -3,20 +3,35 @@ const net = require('net');
 const HOST = '127.0.0.1';
 const PORT = 8080;
 
-const answer = {
-  list: [],
+const connectionList = [];
+const arrClient = []
+
+const command = {
+  list: connectionList.toString(),
   end: 'end',
   hello: 'world',
-  help: Object.keys(this),
-  start_message: 'enter address',
+  startmessage: 'enter address',
 };
+command.help = `${Object.keys(command)}`;
+
+const updateData = () => {
+  command.list = connectionList.toString();
+};
+
+const idRegExp = /\d{1,}\.\d{1,}\.\d{1,}\.\d{1,}:\d{4,}/;
 
 const server = net.createServer((client) => {
   console.log(`CONNECTED: ${client.remoteAddress}:${client.remotePort}`);
 
-  answer.list.push(`${client.remoteAddress}:${client.remotePort}`);
+  connectionList.push(`${client.remoteAddress}:${client.remotePort}`);
+  arrClient.push(client);
+  updateData();
+
   client.on('data', (data) => {
-    const result = `${answer[data.toString()]}` || 'incorrect input';
+    if (data.toString().match(idRegExp)) {
+      
+    }
+    const result = command[data.toString()] || 'incorrect input';
     client.write(result);
 
     server.close();
